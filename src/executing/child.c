3df22417db1f51bcaf074f3e0e6ce44af2196fbc
pstrohal <pstrohal@student.42heilbronn.de>
2024-07-15 18:45:17 +0200

    got it working with piping to the end, was just the stupid printf buffer

--- src/executing/child.c
+++ src/executing/child.c
@@ -6,7 +6,7 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 18:37:42 by pstrohal          #+#    #+#             */
-/*   Updated: 2024/07/15 17:00:56 by pstrohal         ###   ########.fr       */
+/*   Updated: 2024/07/15 18:40:55 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,24 @@
 
 void	change_std_fd(t_pipe *pipes, int mode)
 {
-	int	pid = getpid();
 	if (mode == START)
 	{
-		if (dup2(pipes->out[WRITE], STDOUT_FILENO) < 0)
-			ft_error("dup2 failed\n", ERR_DUP2);
-		if (close(pipes->out[WRITE]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->out[WRITE]);
-		if (close(pipes->out[READ]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->out[READ]);
+		close(pipes->pipe[READ]);
+		dup2(pipes->pipe[WRITE], STDOUT_FILENO);
+		close(pipes->pipe[WRITE]);
 	}
 	else if (mode == MIDDLE)
 	{
-
-		if (dup2( pipes->in[READ], STDIN_FILENO) < 0)
-			ft_error("dup2 failed\n", ERR_DUP2);
-
-		if (dup2(pipes->out[WRITE], STDOUT_FILENO) < 0)
-			ft_error("dup2 failed\n", ERR_DUP2);
-		
-		if (close(pipes->in[READ]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->in[READ]);
-			
-		if (close(pipes->out[WRITE]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->out[WRITE]);
-			
-		if (close(pipes->in[WRITE]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->in[WRITE]);
-
-		if (close(pipes->out[READ]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->out[READ]);
+		close(pipes->pipe[READ]);
+		dup2(pipes->last_pipe, STDIN_FILENO);
+		close(pipes->last_pipe);
+		dup2(pipes->pipe[WRITE], STDOUT_FILENO);
+		close(pipes->pipe[WRITE]);	
 	}
 	else if (mode == END)
-	{		
-		if (dup2(pipes->in[READ], STDIN_FILENO) < 0)
-			ft_error("dup2 failed\n", ERR_DUP2);
-		
-		if (close(pipes->in[READ]) < 0)
-			printf("%dclosing %d failed!!!\n", pid, pipes->in[READ]);
+	{
+		dup2(pipes->last_pipe, STDIN_FILENO);
+		close(pipes->last_pipe);
 	}
 }
 
@@ -61,22 +41,23 @@ void	run_childprocess(t_cmd *cmd, t_pipe *pipes, char **envp, int mode)
 	cmd = NULL;
 	envp = NULL;
 	char buffer[200];
-	int	i = 0;
 	int	bts_read = 0;
 	if (mode == START)
 	{
 		int fd = open("hallo.txt", O_RDONLY);
-		bts_read = read(fd, &buffer[i], 1);
-		i++;
-		while (bts_read > 0)
-			bts_read = read(fd, &buffer[i++], 1);
+		bts_read = read(fd, &buffer, 199);
+		buffer[bts_read] = '\0';
 		close(fd);
 	}
 	if (mode != START)
-		while (bts_read > 0)
-			bts_read = read(STDIN_FILENO, &buffer[i++], 1);
-	fprintf(stderr,"here is %d buffer contains:%s\n", getpid(), buffer);
+	{
+		bts_read = read(STDIN_FILENO, &buffer, 199);
+		buffer[199] = '\0';
+	}
+	// fprintf(stderr,"here is %d buffer contains:%s\n", getpid(), buffer);
 	printf("%s", buffer);
+	fflush(stdout);
+	// write(STDOUT_FILENO, &buffer, bts_read);
 	if (mode == START)
 		close(STDOUT_FILENO);
 	else if (mode == MIDDLE)
--- src/executing/execute_commands.c
+++ src/executing/execute_commands.c
@@ -6,7 +6,7 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 12:54:15 by timschmi          #+#    #+#             */
-/*   Updated: 2024/07/15 16:59:30 by pstrohal         ###   ########.fr       */
+/*   Updated: 2024/07/15 18:30:18 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,50 @@
 		pipein			pipeout
 */
 
-void	open_pipes(t_pipe *pipes, int mode)
+// void	open_pipes(t_pipe *pipes, int mode)
+// {
+// 	if (mode == START)
+// 	{
+// 		pipes->in[READ] = -1;
+// 		pipes->in[WRITE] = -2;
+// 		if (pipe(pipes->out) < 0)
+// 			ft_error("pipe failed to init", ERR_PIPE);
+// 		return ;
+// 	}
+// 	pipes->in[READ] = pipes->out[READ];
+// 	pipes->in[WRITE] = pipes->out[WRITE];
+// 	if (mode == MIDDLE)
+// 	{
+// 		if (pipe(pipes->out) < 0)
+// 			ft_error("pipe failed to init", ERR_PIPE);
+// 	}
+// 	else if (mode == END)
+// 	{
+// 		pipes->out[READ] = -3;
+// 		pipes->out[WRITE] = -4;
+// 	}
+// 	return ;
+// }
+
+void	close_accordingly(t_pipe *pipes, int mode)
 {
 	if (mode == START)
 	{
-		pipes->in[READ] = -1;
-		pipes->in[WRITE] = -2;
-		if (pipe(pipes->out) < 0)
-			ft_error("pipe failed to init", ERR_PIPE);
-		return ;
+		close(pipes->pipe[WRITE]);
+		pipes->last_pipe = pipes->pipe[READ];
 	}
-	pipes->in[READ] = pipes->out[READ];
-	pipes->in[WRITE] = pipes->out[WRITE];
 	if (mode == MIDDLE)
 	{
-		if (pipe(pipes->out) < 0)
-			ft_error("pipe failed to init", ERR_PIPE);
+		close(pipes->last_pipe);
+		close(pipes->pipe[WRITE]);
+		pipes->last_pipe = pipes->pipe[READ];
 	}
-	else if (mode == END)
+	if (mode == END)
 	{
-		pipes->out[READ] = -3;
-		pipes->out[WRITE] = -4;
+		close(pipes->last_pipe);
 	}
 	return ;
 }
-
 int	*allocate_pid(int nb)
 {
 	int *pid;
@@ -50,37 +69,6 @@ int	*allocate_pid(int nb)
 	if (!pid)
 		ft_error("malloc pid_arr\n", ERR_MALLOC);
 	return (pid);
-}
-
-void	close_accordingly(t_pipe *pipes, int mode)
-{
-	if (mode == START)
-	{
-		if (close(pipes->out[WRITE]) < 0)
-			printf("closing %d failed!!!\n", pipes->out[WRITE]);
-		pipes->out[WRITE] = -4;
-	}
-	if (mode == MIDDLE)
-	{
-		if (close(pipes->out[WRITE]) < 0)
-			printf("closing %d failed!!!\n", pipes->out[WRITE]);
-		pipes->out[WRITE] = -4;
-		if  (close(pipes->in[READ]) < 0)
-			printf("closing %d failed!!!\n", pipes->in[READ]);
-		pipes->in[READ] = -1;
-	}
-	if (mode == END)
-	{
-		if (close(pipes->in[READ]) < 0)
-			printf("closing %d failed!!!\n", pipes->in[READ]);
-		pipes->in[READ] = -1;
-	}
-	return ;
-}
-
-void print_pipes(t_pipe *pipes)
-{
-	printf ("in [%d][%d] out [%d][%d]\n\n", pipes->in[READ], pipes->in[WRITE], pipes->out[READ], pipes->out[WRITE]);
 }
 
 
@@ -99,11 +87,12 @@ void execute_commandline(t_shell *shell)
 	{
 		if (i == shell ->cmd_nb - 1)
 			mode = END;
-		open_pipes(&pipes, mode);
+		if (mode != END && pipe(pipes.pipe) < 0)
+			ft_error("pipe filed\n", ERR_PIPE);
 		pid[i] = fork();
 		if (pid[i] == 0)
 			run_childprocess(&shell->commands[i], &pipes, shell->envp, mode);
-		else if (pid < 0)
+		else if (pid[i] < 0)
 			ft_error("fork failed\n", ERR_FORK);
 		close_accordingly(&pipes, mode);
 		if (mode == START)
@@ -128,3 +117,167 @@ int main (int argc, char **argv, char **envp)
 	execute_commandline(&shell);
 	exit (0);
 }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <sys/wait.h>
+// #include <string.h>
+
+// #define START 0
+// #define MIDDLE 1
+// #define END 2
+
+// typedef struct s_pipe {
+//     int pipe[2];
+//     int last_pipe;
+// } t_pipe;
+
+// typedef struct s_cmd {
+//     // command structure here
+// } t_cmd;
+
+// typedef struct s_shell {
+//     int cmd_nb;
+//     t_cmd *commands;
+//     char **envp;
+// } t_shell;
+
+// void ft_error(const char *msg, int err_code) {
+//     perror(msg);
+//     exit(err_code);
+// }
+
+// void change_std_fd(t_pipe *pipes, int mode) {
+//     if (mode == START) {
+//         close(pipes->pipe[0]); // close read end of current pipe
+//         if (dup2(pipes->pipe[1], STDOUT_FILENO) == -1) {
+//             ft_error("dup2 failed", 1);
+//         }
+//         close(pipes->pipe[1]); // close write end of current pipe
+//     } else if (mode == MIDDLE) {
+//         if (dup2(pipes->last_pipe, STDIN_FILENO) == -1) {
+//             ft_error("dup2 failed", 1);
+//         }
+//         close(pipes->last_pipe); // close last pipe after duplicating
+//         close(pipes->pipe[0]); // close read end of current pipe
+//         if (dup2(pipes->pipe[1], STDOUT_FILENO) == -1) {
+//             ft_error("dup2 failed", 1);
+//         }
+//         close(pipes->pipe[1]); // close write end of current pipe
+//     } else if (mode == END) {
+//         if (dup2(pipes->last_pipe, STDIN_FILENO) == -1) {
+//             ft_error("dup2 failed", 1);
+//         }
+//         close(pipes->last_pipe); // close last pipe after duplicating
+//     }
+// }
+
+// void run_childprocess(t_cmd *cmd, t_pipe *pipes, char **envp, int mode) {
+//     change_std_fd(pipes, mode);
+//     cmd = NULL;  // You should probably do something with cmd
+//     envp = NULL; // You should probably do something with envp
+
+//     char buffer[200];
+//     int bts_read;
+
+//     if (mode == START) {
+//         int fd = open("hallo.txt", O_RDONLY);
+//         if (fd == -1) {
+//             ft_error("open failed", 1);
+//         }
+//         while ((bts_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+//             buffer[bts_read] = '\0';
+//             write(STDOUT_FILENO, buffer, bts_read);
+//         }
+//         if (bts_read == -1) {
+//             close(fd);
+//             ft_error("read failed", 1);
+//         }
+//         close(fd);
+//     } else {
+//         while ((bts_read = read(STDIN_FILENO, buffer, sizeof(buffer) - 1)) > 0) {
+//             buffer[bts_read] = '\0';
+//             write(STDOUT_FILENO, buffer, bts_read);
+//         }
+//         if (bts_read == -1) {
+//             ft_error("read failed", 1);
+//         }
+//     }
+
+//     // Close the pipes if necessary
+//     if (mode == START || mode == MIDDLE) {
+//         close(STDOUT_FILENO);
+//     }
+//     if (mode == MIDDLE || mode == END) {
+//         close(STDIN_FILENO);
+//     }
+//     exit(0);
+// }
+
+// void close_accordingly(t_pipe *pipes, int mode) {
+//     if (mode == START) {
+//         close(pipes->pipe[1]);
+//         pipes->last_pipe = pipes->pipe[0];
+//     } else if (mode == MIDDLE) {
+//         close(pipes->last_pipe);
+//         close(pipes->pipe[1]);
+//         pipes->last_pipe = pipes->pipe[0];
+//     } else if (mode == END) {
+//         close(pipes->last_pipe);
+//     }
+// }
+
+// int *allocate_pid(int nb) {
+//     int *pid = (int *)malloc(sizeof(int) * nb);
+//     if (!pid) {
+//         ft_error("malloc pid_arr", 1);
+//     }
+//     return pid;
+// }
+
+// void execute_commandline(t_shell *shell) {
+//     int i;
+//     int *pid;
+//     t_pipe pipes;
+//     int mode;
+//     int t[100];
+
+//     pid = allocate_pid(shell->cmd_nb);
+//     mode = START;
+//     i = -1;
+//     while (++i < shell->cmd_nb) {
+//         if (i == shell->cmd_nb - 1)
+//             mode = END;
+//         if (mode != END && pipe(pipes.pipe) < 0) {
+//             ft_error("pipe failed", 1);
+//         }
+//         pid[i] = fork();
+//         if (pid[i] == 0) {
+//             run_childprocess(&shell->commands[i], &pipes, shell->envp, mode);
+//         } else if (pid[i] < 0) {
+//             ft_error("fork failed", 1);
+//         }
+//         close_accordingly(&pipes, mode);
+//         if (mode == START)
+//             mode = MIDDLE;
+//     }
+//     i = -1;
+//     while (++i < shell->cmd_nb) {
+//         waitpid(pid[i], &t[i], 0);
+//     }
+//     free(pid);
+// }
+
+// int main(int argc, char **argv, char **envp) {
+//     t_shell shell;
+//     argc = 5;
+//     argv = NULL;
+
+//     shell.cmd_nb = 10;
+//     shell.envp = envp;
+//     // Initialize shell.commands here
+//     execute_commandline(&shell);
+//     exit(0);
+// }
