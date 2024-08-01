9a1da852eb9fb210c8f1ee37fe85e914dea3992c
Timur Schmidt <timschmi@2-E-8.42heilbronn.de>
2024-08-01 12:40:55 +0200

    fixed issue with expanding inside of quotes

--- src/tokenizing/token_list.c
+++ src/tokenizing/token_list.c
@@ -6,7 +6,7 @@
 /*   By: timschmi <timschmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 15:07:13 by timschmi          #+#    #+#             */
-/*   Updated: 2024/07/31 15:48:40 by timschmi         ###   ########.fr       */
+/*   Updated: 2024/08/01 12:40:25 by timschmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int	check_variable(char *str, int q_flag)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] == 39)
-			return (IN_QUOTES);
 		if (str[i] == '$' && (!is_whitespace(str[i + 1]) && str[i + 1]))
 			return (VARIABLE);
 		i++;
